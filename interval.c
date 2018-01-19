bf2ff57fc1fd629ab4abce038f95de665f1e1e56
rasigasammu <31506333+rasigasammu@users.noreply.github.com>
2018-01-19 16:25:00 +0530

    Create interval.c

---
+++ interval.c
@@ -0,0 +1,17 @@
+#include<stdio.h>
+#include<conio.h>
+void main()
+{
+ int a,b,i;
+ printf("enter the first interval /n");
+ scanf("%d",&a);
+ printf("enter the second interval /n");
+ scanf("%d",&b);
+ for(i=a,i<b,i++)
+ {
+ if(i%2==0)
+ {
+ printf("The even number is %d",i);
+ }
+ }
+ }
