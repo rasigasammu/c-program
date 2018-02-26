29dae09d7db4150234f0153e636edf033c00ae5a
rasigasammu <31506333+rasigasammu@users.noreply.github.com>
2018-02-26 10:49:30 +0000

    Create fibo.c

---
+++ fibo.c
@@ -0,0 +1,17 @@
+#include<stdio.h>
+int main()
+{
+	int r1=0,r2=1,r3,n;
+	printf("ENTER THE NO.OF.ELEMENTS:\n");
+	scanf("%d",&n);
+	printf("%d",r2);
+	while(n!=0)
+	{
+		r3=r1+r2;
+		printf("\n%d",r3);
+		r1=r2;
+		r2=r3;
+		n--;
+	}
+  return 0;
+	}
