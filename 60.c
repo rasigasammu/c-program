b6baf3fbd8b3af1a35b7a39848898e80d3fdb622
rasigasammu <31506333+rasigasammu@users.noreply.github.com>
2018-03-14 10:57:16 +0000

    Create 60.c

---
+++ 60.c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+int main()
+{
+	int r1=0,r2=1,r3,n;
+	printf("value of n:\n");
+	scanf("%d",&n);
+	printf("%d",r2);
+	while(n!=0)
+	{
+		r3=r1+r2;
+		printf("\n%d",r3);
+		r1=r2;
+		r2=r3;
+	}
+return 0;
+}
